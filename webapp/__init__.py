de029860380348dcf26e5822f0917ab7ef302773
Alex Forsythe <awforsythe@gmail.com>
2019-05-08 16:07:04 -0500

    Add psycopg2 and test locally with Postgres, prepare for Heroku

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -17,6 +17,20 @@ instance_config_path = os.path.join(os.path.dirname(__file__), '..', 'instance',
 if os.path.isfile(instance_config_path):
     app.config.from_pyfile(instance_config_path)
 
+# Merge in config from select environment variables
+for bool_key in ('DEBUG'):
+    str_value = os.getenv(bool_key)
+    if str_value and str_value.lower() in ('true', 'false'):
+        app.config[bool_key] = str_value.lower() == 'true'
+
+for str_key in ('SQLALCHEMY_DATABASE_URI', 'SECRET_KEY', 'DATABASE_URL'):
+    str_value = os.getenv(str_key)
+    if str_value:
+        app.config[str_key] = str_value
+
+if 'DATABASE_URL' in app.config:
+    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['DATABASE_URL']
+
 # Initialize extensions
 db = SQLAlchemy(app)
 rest = Api(app)
